e95e02379f213f16c6d6d424818b584810b4f8a5
nuryan-admin <misa7w7@gmail.com>
2026-04-18 23:11:32 -0600

    Estructura inicial del frontend MVC

---
+++ ProyectoFinal.Progra3.Frontend/Progra3.ProyectoFinal.Frontend/Progra3.ProyectoFinal.Frontend/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Progra3.ProyectoFinal.Frontend.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+# Proyecto Final Backend 
+
+##Integrantes del Proyecto
+* **Abner Paredes** - 2294-23-17751 
